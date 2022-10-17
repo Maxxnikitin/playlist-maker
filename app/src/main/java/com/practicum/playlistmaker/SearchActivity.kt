80e8faf6ab4b99338aafc2c844852ae38d7d2d58
Maxx Nikitin <maxxnikitin90@yandex.ru>
2022-10-17 13:51:46 +0700

    feat: add searh page

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -2,10 +2,46 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val searchInput = findViewById<EditText>(R.id.search_input)
+        val cleanBtn = findViewById<ImageView>(R.id.clear_icon)
+
+        cleanBtn.setOnClickListener {
+            searchInput.setText("")
+        }
+
+        val searchTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                cleanBtn.visibility = clearBtnVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // TODO("Not yet implemented")
+            }
+        }
+
+        searchInput.addTextChangedListener(searchTextWatcher)
+    }
+
+    private fun clearBtnVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
