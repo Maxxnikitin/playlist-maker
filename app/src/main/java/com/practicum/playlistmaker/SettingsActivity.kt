3382243d729aaaf92f391b8f39c2950a129ace71
Maxx Nikitin <maxxnikitin90@yandex.ru>
2022-10-17 12:42:37 +0700

    feat: add intents in settings

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,45 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val shareBtn = findViewById<FrameLayout>(R.id.share_btn)
+        val supportBtn = findViewById<FrameLayout>(R.id.support_btn)
+        val agreementBtn = findViewById<FrameLayout>(R.id.agreement_btn)
+
+        shareBtn.setOnClickListener {
+            val text = "https://practicum.yandex.ru/android-developer/"
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.putExtra(Intent.EXTRA_TEXT, text)
+            intent.type = "text/plain"
+            startActivity(intent)
+        }
+
+        supportBtn.setOnClickListener {
+            val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("maxxnikitin90@yandex.ru"))
+            intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+
+            startActivity(intent)
+        }
+
+        agreementBtn.setOnClickListener {
+            val url = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            val intent = Intent(Intent.ACTION_VIEW, url)
+            startActivity(intent)
+        }
     }
 }
